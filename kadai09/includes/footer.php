b80308b852807c24caf4f46890533d735f3c4b65
aiko <aikohayashi1224@gmail.com>
2026-07-24 11:07:39 +0900

    誤字修正

--- kadai09/includes/footer.php
+++ kadai09/includes/footer.php
@@ -1,6 +1,6 @@
     </div><!-- /.container -->
     <footer class="site-footer">
-        <p>&copy; 2025 エンディングノート</p>
+        <p>&copy; エンディングノート</p>
     </footer>
     <script src="/gs_kadai/kadai09/js/main.js"></script>
 </body>
